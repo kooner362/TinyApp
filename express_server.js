85d24a93434133dafef2fb88d12823202c4996f4
Gurp <kooner@gmail.com>
2019-03-12 19:14:51 +0000

    Finished POST method for urls so its appends to urlDatabase and redirects

--- express_server.js
+++ express_server.js
@@ -29,8 +29,10 @@ app.get("/urls/", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  var longURL = req.body.longURL;
+  var shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls/' + shortURL);
 });
 
 app.get("/urls/new", (req, res) => {
