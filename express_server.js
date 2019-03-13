04ffb78717839a3fbd729c083684a578a1d5a067
Gurp <kooner@gmail.com>
2019-03-13 18:06:45 +0000

    Completed forms for registration

--- express_server.js
+++ express_server.js
@@ -27,6 +27,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/register", (req, res) => {
+  res.render('urls_register');
+});
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
