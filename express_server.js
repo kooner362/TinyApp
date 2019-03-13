df00b52cdc163487e765ae5e0c515f073381c677
Gurp <kooner@gmail.com>
2019-03-13 00:06:12 +0000

    finished login/logout

--- express_server.js
+++ express_server.js
@@ -33,7 +33,7 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase , username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
@@ -50,11 +50,14 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = {shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -66,7 +69,13 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username).send('cookie-set');
+  res.cookie("username", req.body.username);
+  res.redirect('/urls/');
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect('/urls/');
 });
 
 function generateRandomString() {
